3b38687bb0356a0119b154510317f2d7f2810d12
Yufan CHEN <108385820+CYF2000127@users.noreply.github.com>
2026-03-26 23:15:49 +0800

    Update get_R_group_sub_agent.py

--- get_R_group_sub_agent.py
+++ get_R_group_sub_agent.py
@@ -1029,7 +1029,7 @@ def process_reaction_image_with_product_variant_R_group(image_path: str) -> dict
     """
  
     client = AzureOpenAI(
-        api_key=API_KEY,
+        =,
         api_version=API_VERSION,
         azure_endpoint=AZURE_ENDPOINT
     )
@@ -1314,10 +1314,7 @@ def process_reaction_image_with_product_variant_R_group_OS(
     *,
     model_name: str = "/models/Qwen3-VL-32B-Instruct-AWQ",
     base_url: Optional[str] = "http://localhost:8000/v1",
-    api_key: Optional[str] = None,
-    # model_name="gemini-2.5-flash",
-    # base_url="https://generativelanguage.googleapis.com/v1beta/openai/", 
-    # api_key="AIzaSyBL8j4MbHAPhq8cR4Y05o9tY5Zq6fMDU3g"  
+    : Optional[str] = None,
 ) -> dict:
     """
     与 process_reaction_image_with_product_variant_R_group 流程保持一致，但改用兼容 OpenAI Chat Completions 协议的本地/自建模型（如 vLLM 或 Ollama）。
@@ -1845,9 +1842,7 @@ def process_reaction_image_with_table_R_group_OS(
     model_name: str = "/models/Qwen3-VL-32B-Instruct-AWQ",
     base_url: Optional[str] = "http://localhost:8000/v1",
     api_key: Optional[str] = None,
-    # model_name="gemini-2.5-flash",
-    # base_url="https://generativelanguage.googleapis.com/v1beta/openai/", 
-    # api_key="AIzaSyBL8j4MbHAPhq8cR4Y05o9tY5Zq6fMDU3g"  
+
 ) -> dict:
     """
     与 process_reaction_image_with_table_R_group 流程保持一致，但改用兼容 OpenAI Chat Completions 协议的本地/自建模型（如 vLLM 或 Ollama）。
