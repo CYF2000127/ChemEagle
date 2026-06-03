716ceb13175d0758aaf4687cbabd8836e13f6064
Yufan CHEN <108385820+CYF2000127@users.noreply.github.com>
2026-06-04 03:20:32 +0800

    Update constants.py

--- molnextr/constants.py
+++ molnextr/constants.py
@@ -1,6 +1,7 @@
 from typing import List
 import re
 
+
 ORGANIC_SET = {'B', 'C', 'N', 'O', 'P', 'S', 'F', 'Cl', 'Br', 'I'}
 
 RGROUP_SYMBOLS = ['R', 'R1', 'R2', 'R3', 'R4', 'R5', 'R6', 'R7', 'R8', 'R9', 'R10', 'R11', 'R12',
