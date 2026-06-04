22ee2557492202feecd702beb33bd9c4be488eab
Yufan CHEN <108385820+CYF2000127@users.noreply.github.com>
2026-06-04 17:43:35 +0800

    Update get_R_group_sub_agent.py

--- get_R_group_sub_agent.py
+++ get_R_group_sub_agent.py
@@ -1031,7 +1031,6 @@ def process_reaction_image_with_product_variant_R_group(image_path: str) -> dict
     """
  
     client = AzureOpenAI(
-        =
         api_key=API_KEY,
         api_version=API_VERSION,
         azure_endpoint=AZURE_ENDPOINT
